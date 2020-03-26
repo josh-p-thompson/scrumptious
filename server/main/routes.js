a36da00653fb8252f83a71d0e371f26cebd2a396
Josh Thompson <jpthompson93@gmail.com>
2020-03-25 23:04:08 -0700

    Created postgres database

--- server/main/routes.js
+++ server/main/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('./db')
 
 const restaurantsData = require('../static/example_restaurants.json');
 const articlesData = require('../static/example_articles.json');
