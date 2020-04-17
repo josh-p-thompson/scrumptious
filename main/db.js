38faec245723c34c0952d00521ebe9a0971314dd
Josh Thompson <jpthompson93@gmail.com>
2020-04-16 20:51:07 -0700

    Debug heroku
    Debuggingggg

--- main/db.js
+++ main/db.js
@@ -1,19 +1,21 @@
 const { Pool } = require('pg')
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'scrumptious',
-//   password: '',
-//   post: 5432
-// })
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'scrumptious',
+  password: '',
+  post: 5432
 })
 
+if (isProduction) {
+  const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  })
+}
+
 module.exports = { pool }
-
-
-// module.exports = pool
