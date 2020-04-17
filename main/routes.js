19a1fc467907f5948ad290ab73a9b3e4edc3ed0b
Josh Thompson <jpthompson93@gmail.com>
2020-04-16 21:26:30 -0700

    Debug heroku
    Debug heroku

--- main/routes.js
+++ main/routes.js
@@ -50,19 +50,6 @@ ROUTES
 */
 
 
-if (process.env.NODE_ENV === 'production') {
-	// Exprees will serve up production assets
-	// router.use(express.static('client/build'));
-  
-	// Express serve up index.html file if it doesn't recognize route
-	const path = require('path');
-	router.get('*', (req, res) => {
-	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
-
-
 router.get('/api/restaurants', (request, response) => {
 	let userLat = request.query.lat;
 	let userLng = request.query.lng;
